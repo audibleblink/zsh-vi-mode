3bde873a83ad1a5f05e686ae4670266affc0c591
Alex Flores <domain@me.com>
2024-02-20 16:23:29 -0500

    adds vim nav in completion menu

--- zsh-vi-mode.zsh
+++ zsh-vi-mode.zsh
@@ -3712,3 +3712,11 @@ case $ZVM_INIT_MODE in
   *) precmd_functions+=(zvm_init);;
 esac
 
+
+# navigate completions with hjkl
+zmodload zsh/complist
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'o' accept-and-infer-next-history
